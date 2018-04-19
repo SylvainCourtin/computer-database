0acd2a25b5201188c7f61dfc3c8fa2b5408cee38
sylvaincourtin <scourtin@excilys.com>
2018-04-19 17:17:23 +0200

    [BUG] correction d'un bug qui lors du choix de computer on faisait un where sur company et non computer (oops j'écris mal x) )

--- src/com/excilys/computerdatabase/utils/MyConstants.java
+++ src/com/excilys/computerdatabase/utils/MyConstants.java
@@ -7,7 +7,7 @@ public class MyConstants {
 	public static final String SQL_QUERY_COMPANY_COUNT = "SELECT COUNT(*) FROM company;";
 
 	public static final String SQL_QUERY_COMPUTER_SELECT_LEFT_JOIN_COMPANY_LIMIT = "SELECT * FROM computer LEFT JOIN company ON company.id = computer.company_id LIMIT ? OFFSET ? ;";
-	public static final String SQL_QUERY_COMPUTER_SELECT_LEFT_JOIN_COMPANY = "SELECT * FROM computer LEFT JOIN company ON company.id = computer.company_id WHERE company_id=?;";
+	public static final String SQL_QUERY_COMPUTER_SELECT_LEFT_JOIN_COMPANY = "SELECT * FROM computer LEFT JOIN company ON company.id = computer.company_id WHERE computer.id=?;";
 	public static final String SQL_QUERY_COMPUTER_INSERT = "INSERT INTO computer (name,introduced,discontinued,company_id) VALUES (?,?,?,?);";
 	public static final String SQL_QUERY_COMPUTER_DELETE = "DELETE FROM computer WHERE id=";
 	public static final String SQL_QUERY_COMPUTER_UPDATE = "UPDATE computer SET name=?, introduced=?, discontinued=?, company_id=? WHERE id=?;";
